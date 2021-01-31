3f5d90d63eb2040e169b1f38c4a86be0a2a527a5
Diego Stiehl <seccomiro@gmail.com>
2021-01-31 18:35:15 -0300

    Swap persistence to localStorage

--- renderer/database.js
+++ renderer/database.js
@@ -3,20 +3,33 @@ const newObject = description => {
   return { id, description };
 };
 
-const data = [newObject('Sample 1'), newObject('Sample 2')];
-
 exports.save = description => {
+  const data = this.all();
   data.push(newObject(description));
+
+  localStorage.setItem('data', JSON.stringify(data))
+
   return this.all();
 };
 
 exports.all = () => {
+  const jsonData = localStorage.data;
+  if (!jsonData) {
+    data = [];
+  } else {
+    data = JSON.parse(jsonData);
+  }
+
   return data;
 };
 
 exports.delete = id => {
+  const data = this.all();
+
   const index = data.findIndex(item => item.id == id);
 
   data.splice(index, 1);
+  localStorage.setItem('data', JSON.stringify(data))
+
   return this.all();
 };
